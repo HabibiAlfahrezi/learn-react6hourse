7456432c7e2dbb89d75dd37cb18a8d2245f6b3e6
Habibi Alfahrezi <habibialfahrezi6@gmail.com>
2024-10-01 08:58:28 +0700

    mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,93 @@
-
-import { useState } from 'react';
-import Employee from './components/Employee'
-
+import "./style.css";
+import { useState } from "react";
+import Employee from "./components/Employee";
 function App() {
-  const [role, setRole] = useState("")
-
-  const showEmployee = true;
-  return (
-    <>
-    {console.log('Inside the return')}
-      {showEmployee 
-      ? 
-      <div>
-        <input type="text" onChange={(e) => {
-          setRole(e.target.value)
-        }} />
-        <Employee name="Habibi Alfahrezi" role="Intern"/> 
-        <Employee name="Royan" role={role}/>
-        <Employee name="Sarman"/>
-      </div>
-      : 
-      <p>You cannont see the employee</p>
-      }
-    </>
-  )
+	const [role, setRole] = useState("");
+	const [employees, setEmployees] = useState([
+		{
+			id: 1,
+			name: "Habibi Alfahrezi",
+			role: "Intern",
+			img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 2,
+			name: "Royan",
+			role: "Web Developer",
+			img: "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 3,
+			name: "Sarman",
+			role: "Designer",
+			img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 4,
+			name: "Sobri",
+			role: "Software Developer",
+			img: "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 5,
+			name: "Elpan",
+			role: "DevOps",
+			img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 6,
+			name: "Dika",
+			role: "Backend Developer",
+			img: "https://images.pexels.com/photos/3775532/pexels-photo-3775532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 7,
+			name: "Fatimah",
+			role: "Data Scientist",
+			img: "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 8,
+			name: "Aulia",
+			role: "Frontend Developer",
+			img: "https://images.pexels.com/photos/289667/pexels-photo-289667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 9,
+			name: "Rahmat",
+			role: "Quality Assurance",
+			img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		},
+		{
+			id: 10,
+			name: "Syifa",
+			role: "Project Manager",
+			img: "https://images.pexels.com/photos/709879/pexels-photo-709879.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+		}
+	]);
+	const showEmployee = true;
+	return (
+		<div className="">
+			{console.log("Inside the return")}
+			{showEmployee ? (
+				<div>
+					<input
+						type="text"
+						onChange={(e) => {
+							setRole(e.target.value);
+						}}
+					/>
+					<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-center">
+						{employees.map((user) => {
+							return <Employee key={user.id} user={user} />;
+						})}
+					</div>
+				</div>
+			) : (
+				<p>You cannont see the employee</p>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,13 +1,34 @@
-const Employee = (props) => {
+import PropTypes from "prop-types";
+
+const Employee = ({user}) => {
 	return (
-		<>
-			<h3>Employee {props.name}</h3>
-			<p>{props.role ? props.role : 'This Employee Has No Role'}</p>
-		</>
+		<div className="m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+			<img
+				className="object-cover  h-[100px] w-[100px] block mx-auto  rounded-full sm:mx-0 sm:shrink-0"
+				src={user.img}
+				alt="Woman's Face"
+			/>
+			<div className="text-center space-y-2 sm:text-left">
+				<div className="space-y-0.5">
+					<p className="text-lg text-black font-semibold">{user.name}</p>
+					<p className="text-slate-500 font-medium">{user.role ? user.role : "This employee didnt have role"}</p>
+				</div>
+				<button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+					Update
+				</button>
+			</div>
+		</div>
 	);
 };
 
+Employee.propTypes = {
+	user: PropTypes.shape({
+		id: PropTypes.number,
+		img: PropTypes.string,
+		name: PropTypes.string,
+		role: PropTypes.string,
+	})
+
+}
 
 export default Employee;
-
-
